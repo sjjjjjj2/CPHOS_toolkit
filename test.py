e8f9382cde3a79cc8a1daee698567c93416842fd
jz shi <shi-jz21@mails.tsinghua.edu.cn>
2022-10-31 00:57:10 +0800

    better_test_example

--- test.py
+++ test.py
@@ -3,18 +3,20 @@ from db_api.DataQueryApis.GetTeacherInfoApis import *
 from db_api.DataManagingApis.ChangeTeacherInfoApis import *
 
 returned = customTransaction.executeOperation(GetTeacherInfoByName("史景喆"))
-print(returned)
-
-
-
+print('customTransaction.executeOperation(GetTeacherInfoByName("史景喆")) returned: ', returned)
 sjzid = returned[0]['id']
 returned = customTransaction.executeOperation(GetTeacherInfoByName("史景喆的小号"))
-# print(returned)
+print('customTransaction.executeOperation(GetTeacherInfoByName("史景喆的小号")) returned: ', returned)
 sjzxhid = returned[0]['id']
+
+
+
 testreturned = customTransaction.executeOperation(MakeAllTypesToBeSubCoach(sjzxhid,sjzid))
+print("customTransaction.executeOperation(MakeAllTypesToBeSubCoach(sjzxhid,sjzid)) returned:", testreturned)
+
 
 returned = customTransaction.executeOperation(GetTeacherInfoByName("史景喆的小号"))
+print("now customTransaction.executeOperation(GetTeacherInfoByName('史景喆的小号')) returned:", returned)
 
-returned = customTransaction.executeOperation(GetTeacherInfoByName("史景喆的小号"))
-print(returned)
-customTransaction.commit()
+
+customTransaction.rollBack() #rollBack, not making any change to the database.
